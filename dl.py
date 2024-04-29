b2609f590fd117d2df76321c8f607840a420d171
i <i@is-MacBook-Air.home>
2024-04-29 17:28:03 +0200

    upd website

--- dl.py
+++ dl.py
@@ -1,8 +1,12 @@
 import os
 from wpd import WebPageDownloader
 
-url = "https://ivandkotelnikov.wixsite.com/test"
+url = "https://ivandkotelnikov.wixsite.com/test/"
+pages = ['pregnant', 'channel', 'lectures', 'consulting']
 data_dir = os.getcwd() + '/sample'
-print(data_dir)
 wpd = WebPageDownloader(url, data_dir)
 wpd.save_all_assets()
+
+for p in pages:
+    wpd = WebPageDownloader(url + p, data_dir + '/' + p)
+    wpd.save_all_assets()
