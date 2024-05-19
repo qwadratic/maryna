5ee09c3160ef726c4a0e48bab1936e31d62f4f57
i <i@is-MacBook-Air.home>
2024-05-19 18:36:12 +0200

    test payments

--- sample/js/mono.js
+++ sample/js/mono.js
@@ -1,15 +1,22 @@
 const checkoutBtnId = 'comp-lvgy43u7';
+const testBtnId = 'comp-lvgy43u6';
 const apiToken = 'mPnuOYfOfRnFp9U67jCWxaw';
 const url = 'https://api.monobank.ua/api/merchant/invoice/create';
-const redirectUrl = 'https://pepamema.baby/';
+const redirectUrl = 'https://t.me/+hcWHak-AgwE1NWQy';
 
 window.addEventListener('load', function() {
-    const button = document.getElementById(checkoutBtnId);
-  
-    button.onclick = function() {
+    const button1 = document.getElementById(checkoutBtnId);
+    const button2 = document.getElementById(testBtnId);
+
+    button1.onclick = function() {
       const data = {
-        amount: 1000,
+        amount: 44900,
         redirectUrl: redirectUrl,
+        merchantPaymInfo: {
+            destination: "За iнформацiйнi послуги",
+            comment: "11111",
+            reference: "222222"
+        }
       };
   
       const options = {
@@ -32,5 +39,37 @@ window.addEventListener('load', function() {
           }
         });
     };
+
+    button2.onclick = function() {
+        const data = {
+          amount: 10000000,
+          redirectUrl: redirectUrl,
+          merchantPaymInfo: {
+              destination: "За iнформацiйнi послуги",
+              comment: "33333",
+              reference: "44444"
+          }
+        };
+    
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Token': apiToken
+          },
+          body: JSON.stringify(data)
+        };
+    
+        fetch(url, options)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            console.log('Success:', data);
+            if (data.pageUrl) {
+              window.location.href = data.pageUrl;
+            }
+          });
+      };
 });
   
