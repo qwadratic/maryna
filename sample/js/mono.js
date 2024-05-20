c2fb0e169b1f13e88f6736ca43af7e667d9e6240
i <i@is-MacBook-Air.home>
2024-05-20 19:13:40 +0200

    final

--- sample/js/mono.js
+++ sample/js/mono.js
@@ -1,16 +1,14 @@
-const checkoutBtnId = 'comp-lvgy43u7';
-const testBtnId = 'comp-lvgy43u6';
+const checkoutBtnId = 'comp-lvgy43u42';
 const apiToken = 'mPnuOYfOfRnFp9U67jCWxaw';
 const url = 'https://api.monobank.ua/api/merchant/invoice/create';
 const redirectUrl = 'https://t.me/+hcWHak-AgwE1NWQy';
 
 window.addEventListener('load', function() {
-    const button1 = document.getElementById(checkoutBtnId);
-    const button2 = document.getElementById(testBtnId);
-
-    button1.onclick = function() {
+    const button = document.getElementById(checkoutBtnId);
+    console.log('####')
+    button.onclick = function() {
       const data = {
-        amount: 1000,
+        amount: 44900,
         redirectUrl: redirectUrl,
         merchantPaymInfo: {
             destination: "За iнформацiйнi послуги",
@@ -27,7 +25,8 @@ window.addEventListener('load', function() {
         },
         body: JSON.stringify(data)
       };
-  
+      
+      console.log('click')
       fetch(url, options)
         .then(response => {
           return response.json();
@@ -40,36 +39,5 @@ window.addEventListener('load', function() {
         });
     };
 
-    button2.onclick = function() {
-        const data = {
-          amount: 10000000,
-          redirectUrl: redirectUrl,
-          merchantPaymInfo: {
-              destination: "За iнформацiйнi послуги",
-              comment: "33333",
-              reference: "44444"
-          }
-        };
-    
-        const options = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Token': apiToken
-          },
-          body: JSON.stringify(data)
-        };
-    
-        fetch(url, options)
-          .then(response => {
-            return response.json();
-          })
-          .then(data => {
-            console.log('Success:', data);
-            if (data.pageUrl) {
-              window.location.href = data.pageUrl;
-            }
-          });
-      };
 });
   
